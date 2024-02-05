813c0430ed1fc22f6137d8c96377a3b5bd1840c0
sunnymasih0253 <sunnyk.mdb@gmail.com>
2024-02-05 09:48:53 +0530

    add reward pages

--- pages/customer/profile/rewards.tsx
+++ pages/customer/profile/rewards.tsx
@@ -1,6 +1,6 @@
 import SideBarLayout from '@/components/layout/sideBarLayout'
 import Referal from '@/components/profile/referal'
-import React from 'react'
+import React, { useState } from 'react'
 import { getProviders } from "next-auth/react"
 import { getServerSession } from "next-auth/next"
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
@@ -19,11 +19,41 @@ interface propsData {
 }
 
 const Refer = (props: propsData) => {
-
+    const [active,setActive] = useState(1);
     console.log(props.rewardsList,'-------------rewards List');
     return (
         <SideBarLayout userDetail={props.userDetail} referalList={props.referalList} eventList={props.eventList}>
             <ToastContainer />
+            <div className='p-5 md:p-40'>
+                <h3 className='sec-title'>All Rewards</h3>
+                <div className='flex items-center gap-[40px] mt-[40px]'>
+                    <button onClick={()=>{setActive(1)}} className={`solid-button !px-[20px] !py-[10px] ${active == 1 ? '' : '!bg-[#5367ff42]'}`}>All Status</button>
+                    <button onClick={()=>{setActive(2)}} className={`solid-button !px-[20px] !py-[10px] ${active == 2 ? '' : '!bg-[#5367ff42]'}`}>Available </button>
+                    <button onClick={()=>{setActive(3)}} className={`solid-button !px-[20px] !py-[10px] ${active == 3 ? '' : '!bg-[#5367ff42]'} `}>Used</button>
+                    <button onClick={()=>{setActive(4)}} className={`solid-button !px-[20px] !py-[10px] ${active == 4 ? '' : '!bg-[#5367ff42]'}`}>Expired</button>
+                </div>
+                <div className='grid grid-cols-2 mt-[40px]'>
+                    <div className='rounded-[10px] bg-white'>
+                        <div className='pl-[24px] py-[30px] pr-[0] rounded-[10px] bg-primary-400 relative z-[1]'>
+                            <div className='flex items-center'>
+                                <div>
+                                    <h3 className='sec-title !text-white'>20 USDT</h3>
+                                    <p className='sm-text !text-white mt-[8px]'>Coupon · Derivatives</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='border border-[#e9edf2] pt-[40px] mt-[-40px] rounded-[10px] p-[24px]'>
+                            <div className='flex items-center mt-[24px] gap-[20px] justify-between'>
+                                <div>
+                                    <div className='bg-[#3844520f] h-[8px] w-full max-w-[100px] rounded-[8px]'></div>
+                                    <p className='sm-text '>Use before 2024-02-10</p>
+                                </div>
+                                <button className='solid-button !px-[20px] !py-[10px]'>Use</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </SideBarLayout>
     )
 }
