50ab232d3bc0b6369c09438f8e38bb5245e4f034
surinder <surinderkumar.mdb@gmail.com>
2024-03-11 18:46:18 +0530

    changes for kyc file upload

--- components/profile/kycAuth.tsx
+++ components/profile/kycAuth.tsx
@@ -87,6 +87,10 @@ const KycAuth = (props: fixSection) => {
   const { data: session, status } = useSession();
   const [btnDisabled, setBtnDisabled] = useState(false);
 
+  const [enableFront, setEnableFront] = useState(false);
+  const [enableBack, setEnableBack] = useState(false);
+  const [enableStatement, setEnableStatement] = useState(false);
+
   const router = useRouter()
 
   const list = [
@@ -144,26 +148,29 @@ const KycAuth = (props: fixSection) => {
       const formData = new FormData();
       formData.append('file', file);
       formData.append('upload_preset', 'my-uploads');
-
+      setEnableFront(true);
       const data = await fetch(`${process.env.NEXT_PUBLIC_FILEUPLOAD_URL}`, {
         method: 'POST',
         body: formData
       }).then(r => r.json());
 
-      if(data.error!==undefined){
+      if (data.error !== undefined) {
         setError("idfront", {
           type: "custom",
           message: data?.error?.message,
         });
+        setEnableFront(false);
         return;
       }
       setFromFrontImg(data.secure_url);
       setFrontImg(data.secure_url);
       setValue("idfront", data.secure_url);
       clearErrors("idfront");
+      setEnableFront(false);
 
     } catch (error) {
       console.error(error);
+      setEnableFront(false);
     }
     // let files = e.target.files[0];
     // setValue("idfront", files);
@@ -192,7 +199,7 @@ const KycAuth = (props: fixSection) => {
         body: formData
       }).then(r => r.json());
 
-      if(data.error!==undefined){
+      if (data.error !== undefined) {
         setError("idback", {
           type: "custom",
           message: data?.error?.message,
@@ -234,7 +241,7 @@ const KycAuth = (props: fixSection) => {
         body: formData
       }).then(r => r.json());
 
-      if(data.error!==undefined){
+      if (data.error !== undefined) {
         setError("statement", {
           type: "custom",
           message: data?.error?.message,
@@ -714,7 +721,14 @@ const KycAuth = (props: fixSection) => {
             <div className="w-full">
               <label className="sm-text ">Identity Document Front Side</label>
 
-              <div className="w-full min-h-[160px] hover:dark:bg-black-v-1 flex  mt-2 md:mt-5 border-[1.5px] border-dashed border-grey-v-1 dark:border-grey-v-2 dark:border-opacity-[15%] rounded-md">
+              <div className="w-full relative min-h-[160px] hover:dark:bg-black-v-1 flex  mt-2 md:mt-5 border-[1.5px] border-dashed border-grey-v-1 dark:border-grey-v-2 dark:border-opacity-[15%] rounded-md">
+                {enableFront &&
+                  <>
+                    <div className="bg-black  z-[1] duration-300 absolute top-0 left-0 h-full w-full opacity-80 visible"></div>
+                    <div className='loader w-[35px] z-[2] h-[35px] absolute top-[calc(50%-10px)] left-[calc(50%-10px)] border-[6px] border-[#ff815d] rounded-full animate-spin border-t-[#ff815d75] '></div>
+                  </>
+                }
+
                 <div className="m-auto ">
                   <input
                     type="file"
@@ -722,14 +736,15 @@ const KycAuth = (props: fixSection) => {
                     id={`front${props?.num}`}
                     name="front"
                     autoComplete="off"
-                    className="hidden "
+                    className={`hidden`}
+                    disabled={(enableBack === true || enableStatement === true) ? true : false}
                     onChange={(e) => {
                       handleFileChange(e);
                     }}
                   />
                   <label
                     htmlFor={`front${props?.num}`}
-                    className="py-[30px]  cursor-pointer block h-full items-stretch "
+                    className={`py-[30px]  ${(enableStatement === true || enableBack === true) ? 'cursor-default' : 'cursor-pointer'} block h-full items-stretch `}
                   >
                     <div className={`${frontImg === "" ? "block" : "hidden"}`}>
                       <p className="nav-text-sm md:nav-text-lg text-center  mb-2">
@@ -767,7 +782,13 @@ const KycAuth = (props: fixSection) => {
 
             <div className="w-full ">
               <label className="sm-text ">Identity Document Back Side</label>
-              <div className="w-full min-h-[160px] mt-2 md:mt-5 border-[1.5px] border-dashed border-grey-v-1 flex dark:border-grey-v-2 dark:border-opacity-[15%] rounded-md">
+              <div className="w-full relative min-h-[160px] mt-2 md:mt-5 border-[1.5px] border-dashed border-grey-v-1 flex dark:border-grey-v-2 dark:border-opacity-[15%] rounded-md">
+                {enableBack &&
+                  <>
+                    <div className="bg-black  z-[1] duration-300 absolute top-0 left-0 h-full w-full opacity-80 visible"></div>
+                    <div className='loader w-[35px] z-[2] h-[35px] absolute top-[calc(50%-10px)] left-[calc(50%-10px)] border-[6px] border-[#ff815d] rounded-full animate-spin border-t-[#ff815d75] '></div>
+                  </>
+                }
                 <div className="m-auto ">
                   <input
                     type="file"
@@ -775,14 +796,15 @@ const KycAuth = (props: fixSection) => {
                     id={`back${props?.num}`}
                     name="back"
                     autoComplete="off"
-                    className="hidden "
+                    className={`hidden`}
+                    disabled={(enableFront === true || enableStatement === true) ? true : false}
                     onChange={(e) => {
                       handleBackChange(e);
                     }}
                   />
                   <label
                     htmlFor={`back${props?.num}`}
-                    className="py-[30px]  cursor-pointer block h-full items-stretch"
+                    className={`py-[30px]  ${(enableStatement === true || enableFront === true) ? 'cursor-default' : 'cursor-pointer'} block h-full items-stretch`}
                   >
                     <div className={`${backImg === "" ? "block" : "hidden"}`}>
                       <p className="nav-text-sm md:nav-text-lg text-center  mb-2">
@@ -827,7 +849,8 @@ const KycAuth = (props: fixSection) => {
                     id={`statement${props?.num}`}
                     name="statement"
                     autoComplete="off"
-                    className="hidden "
+                    className={`hidden `}
+                    disabled={(enableBack === true || enableFront === true) ? true : false}
                     onChange={(e) => {
                       handleSelfieChange(e);
                     }}
@@ -835,7 +858,7 @@ const KycAuth = (props: fixSection) => {
 
                   <label
                     htmlFor={`statement${props?.num}`}
-                    className="py-[30px]  cursor-pointer block h-full items-stretch"
+                    className={`py-[30px]  ${(enableBack === true || enableFront === true) ? 'cursor-default' : 'cursor-pointer'} block h-full items-stretch`}
                   >
                     <div className={`${selfieImg === "" ? "block" : "hidden"}`}>
                       <Image
@@ -885,16 +908,16 @@ const KycAuth = (props: fixSection) => {
               This account was created on January 10, 2022, 02:12 PM
             </p> */}
 
-            <button disabled={btnDisabled}
+            <button disabled={(btnDisabled === true || frontImg === "" || backImg === "" || selfieImg === "") ? true : false}
               type="submit"
-              className="solid-button px-[23px] md:px-[51px]"
+              className={`solid-button px-[23px] md:px-[51px] ${frontImg === "" || backImg === "" || selfieImg === "" ? 'opacity-25 cursor-not-allowed' : ''}`}
             >
               {btnDisabled &&
-              <svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
-                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
-              </svg>
-            }
+                <svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
+                </svg>
+              }
               Submit
             </button>
           </div>
