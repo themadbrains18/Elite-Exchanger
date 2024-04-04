6368152be96516c653c92c87a6b48872691eafac
surinder <surinderkumar.mdb@gmail.com>
2024-04-04 15:34:13 +0530

    change check user auth

--- middleware.ts
+++ middleware.ts
@@ -62,12 +62,11 @@ export default async function middleware(req: NextRequest, res : NextResponse) {
     console.log(session,path, ' ==== session session session session session session session session session ====')
     let role : unknown  = session?.role;
 
-    // if (!session && !path.includes("/login")) {
-    //   return NextResponse.redirect(new URL("/login", req.url));
-    // } else if (session && path == "/login") {
-    //   return NextResponse.redirect(new URL("/", req.url));
-    // }
-    // return NextResponse.redirect(new URL("/", req.url));
+    if (!session && !path.includes("/login")) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    } else if (session && path == "/login") {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
     return NextResponse.rewrite(
       new URL(`/admin${path === "/" ? "" : path}`, req.url),
     );
