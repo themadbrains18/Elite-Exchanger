97f71353d4a66d3076bebab237f29f793f0f994c
surinder <surinderkumar.mdb@gmail.com>
2024-04-15 18:12:02 +0530

    changes price conversion

--- components/watchlist/exchange.tsx
+++ components/watchlist/exchange.tsx
@@ -103,16 +103,30 @@ const Exchange = (props: DynamicId): any => {
 
     if (firstMannual === false || secondMannual === false) {
       setBtnDisabled(true)
-      let priceData = await fetch(`${process.env.NEXT_PUBLIC_BASEURL}/price/exchange?fsym=${firstMannual === true ? 'USDT' : firstCurrency}&tsyms=${secondMannual === true ? 'USDT' : secondCurrency}`, {
-        method: "GET"
-      }).then(response => response.json());
+      // let priceData = await fetch(`${process.env.NEXT_PUBLIC_BASEURL}/price/exchange?fsym=${firstMannual === true ? 'USDT' : firstCurrency}&tsyms=${secondMannual === true ? 'USDT' : secondCurrency}`, {
+      //   method: "GET"
+      // }).then(response => response.json());
+      let priceData = await fetch("https://api.livecoinwatch.com/coins/single", {
+        method: "POST",
+        headers: new Headers({
+          "content-type": "application/json",
+          "x-api-key": `${process.env.NEXT_PUBLIC_PRICE_SINGLE_ASSET_KEY}`,
+        }),
+        body: JSON.stringify({
+          currency: secondCurrency === 'BTCB' ? 'BTC' : secondCurrency === 'BNBT' ? 'BNB' : secondCurrency,
+          code: firstCurrency === 'BTCB' ? 'BTC' : firstCurrency === 'BNBT' ? 'BNB' : firstCurrency,
+          meta: false
+        }),
+      });
 
-      if (firstMannual === true && priceData?.data?.rate === undefined) {
-        currentPrice = selectedToken?.price * priceData?.data?.rate;
+      let priceData2 = await priceData.json();
+
+      if (firstMannual === true && priceData2?.data?.rate === undefined) {
+        currentPrice = selectedToken?.price * priceData2?.data?.rate;
         conversionPrice = amount * currentPrice;
       }
       else if (secondMannual === true) {
-        currentPrice = priceData?.data?.rate / selectedSecondToken?.price;
+        currentPrice = priceData2?.data?.rate / selectedSecondToken?.price;
 
         // console.log(`1 ${firstCurrency} of ${priceData?.data['USDT']} USDT`);
 
@@ -125,8 +139,8 @@ const Exchange = (props: DynamicId): any => {
         conversionPrice = amount * currentPrice;
       }
       else if (firstMannual === false && secondMannual === false) {
-        currentPrice = priceData?.data?.rate;
-        conversionPrice = amount * priceData?.data?.rate;
+        currentPrice = priceData2?.data?.rate;
+        conversionPrice = amount * priceData2?.data?.rate;
       }
     }
     else {
@@ -283,21 +297,21 @@ const Exchange = (props: DynamicId): any => {
           </div>
         </div>
 
-        {isConvert === false ? <button className={` solid-button w-full ${btnDisabled === true ? 'cursor-not-allowed ':''}`} disabled={btnDisabled} onClick={() => {priceConversion();}}>{btnDisabled &&
-                      <svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
-                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
-                      </svg>
-                    }Preview</button>
+        {isConvert === false ? <button className={` solid-button w-full ${btnDisabled === true ? 'cursor-not-allowed ' : ''}`} disabled={btnDisabled} onClick={() => { priceConversion(); }}>{btnDisabled &&
+          <svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
+          </svg>
+        }Preview</button>
           :
           <div className="flex gap-[18px]">
             <button className=" solid-button w-full bg-grey-v-2 !text-primary" onClick={() => setIsConvert(false)}>Cancel</button>
             <button className=" solid-button w-full" disabled={btnDisabled2} onClick={() => sendConvertRequest()}>{btnDisabled2 &&
-                      <svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
-                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
-                      </svg>
-                    }Convert</button>
+              <svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
+              </svg>
+            }Convert</button>
           </div>
         }
       </div>
