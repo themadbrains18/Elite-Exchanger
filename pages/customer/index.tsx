6c100d55e2a8dc27c10c30953697273e09cf25fc
surinder <surinderkumar.mdb@gmail.com>
2024-04-16 17:02:10 +0530

    ip info

--- pages/customer/index.tsx
+++ pages/customer/index.tsx
@@ -43,7 +43,7 @@ export default function Home({ session, coinList }: Session) {
     fetch('https://ipapi.co/json/')
       .then(response => response.json())
       .then(data => {
-        console.log(`IP: ${data.ip}, Location: ${data}`);
+        console.log(data);
       })
       .catch(error => console.error('Error:', error));
   }, [wbsocket])
