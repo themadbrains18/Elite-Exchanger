4f45cf6a237fa3f279b1afd756554b0213cb158e
surindermdb <surinderkumar.mdb@gmail.com>
2024-07-08 18:28:26 +0530

    update in code

--- components/future/order-book/order-book.tsx
+++ components/future/order-book/order-book.tsx
@@ -11,24 +11,16 @@ interface setHeight {
     positionRecord?: any;
 }
 const OrderBookFuture = (props: setHeight) => {
-    const [highlow, setHighlow] = useState<any>(true);
+    const [highlow, setHighlow] = useState<boolean>(true);
 
-    const [prevPrice, setPrevPrice] = useState<number | null>(null);
-    const [show, setShow] = useState(1)
+    const [prevPrice, setPrevPrice] = useState<any>();
 
     useEffect(() => {
-        const currentPrice = props?.currentToken?.token?.price || props?.currentToken?.global_token?.price;
 
-        if (prevPrice !== null && currentPrice !== prevPrice) {
-            setHighlow(currentPrice > prevPrice);
-        }
+setPrevPrice(props?.positionRecord[0])
 
-        setPrevPrice(currentPrice);
-    }, [props?.currentToken]);
+    }, [props?.positionRecord]);
 
-    const currentPrice = props?.currentToken?.token !== null
-        ? truncateNumber(props?.currentToken?.token?.price, 6)
-        : truncateNumber(props?.currentToken?.global_token?.price, 6);
 
 
 
@@ -162,13 +154,13 @@ const OrderBookFuture = (props: setHeight) => {
                                 viewBox="0 0 32 32"
                                 xmlSpace="preserve"
                                 xmlns="http://www.w3.org/2000/svg"
-                                className={`w-3 ${highlow ? 'rotate-180' : ''}`}
+                                className={`w-3 ${prevPrice?.direction==="long" ? 'rotate-180' : ''}`}
                                 xmlnsXlink="http://www.w3.org/1999/xlink"
                             >
                                 <path
                                     clipRule="evenodd"
                                     d="M26.704,20.393  c-0.394-0.39-1.034-0.391-1.428,0l-8.275,8.193V1c0-0.552-0.452-1-1.01-1s-1.01,0.448-1.01,1v27.586l-8.275-8.192  c-0.394-0.391-1.034-0.391-1.428,0c-0.394,0.391-0.394,1.024,0,1.414l9.999,9.899c0.39,0.386,1.039,0.386,1.429,0l9.999-9.899  C27.099,21.417,27.099,20.784,26.704,20.393C26.31,20.003,27.099,20.784,26.704,20.393z"
-                                    className={`${highlow ? 'fill-buy' : 'fill-sell'}`}
+                                    className={`${prevPrice?.direction==="long" ? 'fill-buy' : 'fill-sell'}`}
                                     fillRule="evenodd"
                                 />
                                 <g />
@@ -179,7 +171,7 @@ const OrderBookFuture = (props: setHeight) => {
                                 <g />
                             </svg>
 
-                            <p className={`info-18 ${highlow ? 'text-buy' : 'text-sell'}`}>{currencyFormatter(truncateNumber(currentPrice, 6))}</p>
+                            <p className={`info-18 ${prevPrice?.direction==="long" ? 'text-buy' : 'text-sell'}`}>{currencyFormatter(truncateNumber(prevPrice ? prevPrice?.entry_price:0, 6))}</p>
                         </div>
                         <p className='info-16 !text-black dark:!text-white !text-[14px] underline'>{props?.currentToken?.token !== null ? currencyFormatter(truncateNumber(props?.currentToken?.token?.price, 6)) : currencyFormatter(truncateNumber(props?.currentToken?.global_token?.price, 6))}</p>
                     </div>
