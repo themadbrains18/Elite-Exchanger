c43a1d85cd4cdb13cb1ca6adb20f63ba132dc811
SakshiSethi0702 <sakshisethi.mdb@gmail.com>
2024-09-02 15:09:02 +0530

    update changes

--- components/tradeHistory/withdrawTable.tsx
+++ components/tradeHistory/withdrawTable.tsx
@@ -156,15 +156,15 @@ const WithdrawTable = (props: propsData) => {
             </tr>
           </thead>
           <tbody>
-            {totalRecord && totalRecord?.length > 0 && totalRecord?.map((item: any, index: any) => {
+            {totalRecord && totalRecord?.length > 0 && totalRecord?.map((item: any, index: any) => {           
               return (
                 <tr key={index}>
                   <td className="sticky left-0 bg-white dark:bg-d-bg-primary">
                     <div className="flex gap-2 py-[10px] md:py-[15px] px-0 md:px-[5px] ">
                       <Image src={`${imgSrc ? '/assets/history/Coin.svg' : item.token !== null ? item?.token?.image : item?.global_token?.image}`} className={`${item?.symbol === "XRP" && "bg-white rounded-full "}`} width={30} height={30} alt="coins" onError={() => setImgSrc(true)} />
                       <div className="flex items-start md:items-center justify-center md:flex-row flex-col gap-0 md:gap-[10px]">
-                        <p className="info-14-18 dark:text-white">{item?.token?.fullName}</p>
-                        <p className="info-10-14 !text-primary py-0 md:py-[3px] px-0 md:px-[10px] bg-[transparent] md:bg-grey-v-2 md:dark:bg-black-v-1 rounded-5">{item?.token?.symbol}</p>
+                        <p className="info-14-18 dark:text-white">{item.token !== null ? item?.token?.symbol : item?.global_token?.symbol}</p>
+                        {/* <p className="info-10-14 !text-primary py-0 md:py-[3px] px-0 md:px-[10px] bg-[transparent] md:bg-grey-v-2 md:dark:bg-black-v-1 rounded-5">{item.token !== null ?item?.token?.symbol : item?.global_token?.symbol}</p> */}
                       </div>
                     </div>
                   </td>
