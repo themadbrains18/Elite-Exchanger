5e9b3c790521eb19029838630f639255a1ad1fb6
SakshiSethi0702 <sakshisethi.mdb@gmail.com>
2024-10-01 17:54:29 +0530

    update change

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,16 +5,17 @@ export default function Document() {
     <Html lang="en">
       <Head >
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
-     
-            <link  as="image" href="/assets/home/BannerDark1.webp" />
-            <link as="image" href="/assets/home/BannerLight1.webp" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
 
-        </Head>
-            <body>
-              <Main />
-              <NextScript />
-            </body>
-          </Html>
-          )
+        <link as="image" href="/assets/home/BannerDark1.webp" />
+        <link as="image" href="/assets/home/BannerLight1.webp" />
+
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
